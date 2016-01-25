a16c9b5730afd8adb1d083738236e9878e2bbea7
brian d foy <brian.d.foy@gmail.com>
2016-01-25 14:35:04 -0500

    Update GitHub address

--- lib/Test/HTTPStatus.pm
+++ lib/Test/HTTPStatus.pm
@@ -118,13 +118,9 @@ Apache::Constants, HTTP::SimpleLinkChecker
 
 =head1 SOURCE AVAILABILITY
 
-This source is part of a SourceForge project which always has the
-latest sources in CVS, as well as all of the previous releases.
+This project is in GitHub:
 
-	http://sourceforge.net/projects/brian-d-foy/
-
-If, for some reason, I disappear from the world, one of the other
-members of the project can shepherd this module appropriately.
+	https://github.com/briandfoy/test-httpstatus
 
 =head1 AUTHOR
 
